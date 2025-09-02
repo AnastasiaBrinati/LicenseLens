f8df4cab81dce5f1eaba452b08bcafaff58526eb
brinatia <birillo347@gmail.com>
2025-09-02 11:11:48 +0200

    correzione menu tab

--- dash.py
+++ dash.py
@@ -1,23 +1,23 @@
 import streamlit as st
-from streamlit_option_menu import option_menu
 from tabs import metrics, map_h3, map_choropleth
 
-st.set_page_config(page_title="Dashboard Eventi", layout="wide")
+import streamlit as st
 
-# Menu orizzontale moderno tipo tab
-active_tab = option_menu(
-    menu_title=None,  # nessun titolo sopra le tab
-    options=["Mappa Attività", "Mappa Priorità", "Metriche"],
-    icons=["map", "map-fill", "bar-chart"],  # icone opzionali
-    menu_icon="cast",
-    default_index=0,
-    orientation="horizontal",
+# Configurazione pagina Streamlit
+st.set_page_config(
+    page_title="Dashboard Eventi",
+    layout="wide",
+    initial_sidebar_state="expanded",
 )
 
-# Caricamento solo della tab attiva (indipendente)
-if active_tab == "Mappa Attività":
+# Crea le tab orizzontali - ora 4 tab separate
+tab1, tab2, tab3  = st.tabs(["Mappa Attività", "Mappa Priorità", "Metriche"])
+
+with tab1:
     map_h3.render()
-elif active_tab == "Mappa Priorità":
+
+with tab2:
     map_choropleth.render()
-elif active_tab == "Metriche":
+
+with tab3:
     metrics.render()
